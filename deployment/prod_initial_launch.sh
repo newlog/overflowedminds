10a84190454b0baa8e89677e5750cae97bdf7d7a
newlog <newlog@overflowedminds.net>
2021-04-19 12:10:06 +0200

    Delete some hardcoded paths

--- deployment/prod_initial_launch.sh
+++ deployment/prod_initial_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose -f "$PWD/production.yml" up -d
+docker-compose -f "$PWD/production.yml" up --build -d
 docker-compose -f "$PWD/production.yml" run --rm django python /app/manage.py migrate
 docker-compose -f "$PWD/production.yml" run --rm django python /app/manage.py loaddata superuser
 docker-compose -f "$PWD/production.yml" run --rm django python /app/manage.py loaddata tags
