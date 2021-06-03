c9175dbca2b6c3af1b0096ca468750db0baeea70
newlog <newlog@overflowedminds.net>
2021-06-03 18:45:12 +0800

    Modify GH action to work with ECR

--- deployment/deploy_remote_prod.sh
+++ deployment/deploy_remote_prod.sh
@@ -1,5 +1,5 @@
 DOCKER_REGISTRY="282428440465.dkr.ecr.us-east-1.amazonaws.com"
 
-aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
+#aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 docker-compose -f production.yml build
 docker-compose -f production.yml push
